975f676bc3e0676df5cbbc0ee5347ae0fff16539
Ashis Laha <ashis.laha@walmartlabs.com>
2019-03-25 21:31:10 +0530

    [x] added login VC and Input VC.

---
+++ SuperCart/InputViewController.swift
@@ -0,0 +1,27 @@
+//
+//  InputViewController.swift
+//  SuperCart
+//
+//  Created by Ashis Laha on 3/25/19.
+//  Copyright © 2019 Team A. All rights reserved.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Take Input"
+    }
+    
+    @IBAction func takeHandwrittenImage(_ sender: UIButton) {
+        
+        
+    }
+    
+    @IBAction func chatWithWalmartRobo(_ sender: UIButton) {
+        
+    }
+}
